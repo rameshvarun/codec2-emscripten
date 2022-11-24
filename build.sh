0743b29f550758e2dfd852ab79c10a58b36c5939
Varun Ramesh <varunramesh@live.com>
2022-11-24 10:07:50 -0700

    Version version appears to be compiled properly.

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
-mkdir build_wasm
+#!/usr/bin/env bash
+
+mkdir -p build_wasm
 cd build_wasm
-wasimake cmake ../codec2
-wasimake make
+emcmake cmake ../codec2
+
+emmake make c2enc c2dec
+
+cd ..
+mkdir -p dist/
+cp build_wasm/src/c2dec.js dist
+cp build_wasm/src/c2dec.wasm dist
+cp build_wasm/src/c2enc.js dist
+cp build_wasm/src/c2enc.wasm dist
