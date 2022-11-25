220bf26142cc4ce49d02dd9e83076a2d6cc96774
Varun Ramesh <varunramesh@live.com>
2022-11-24 17:11:21 -0700

    Updated build script

--- build.sh
+++ build.sh
@@ -4,14 +4,24 @@
 emsdk install 3.1.26
 emsdk activate 3.1.26
 
+# Clone codec2
+git clone https://github.com/drowe67/codec2.git
+cd codec2
+git checkout 67f31bce663caef85abb5dd2df62fb996b246c05
+
+# Apply patched changes.
+git apply ../codec2.patch
+cd ..
+
+# Build codec 2
 mkdir -p build_wasm
 cd build_wasm
 emcmake cmake ../codec2
-
 emmake make c2enc c2dec
 
+# Copy built artifacts into build/
 cd ..
-mkdir -p dist/
+mkdir -p build/
 cp build_wasm/src/c2dec.js build
 cp build_wasm/src/c2dec.wasm build
 cp build_wasm/src/c2enc.js build
