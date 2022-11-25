8bdc01ed8c456900519231c853f211a0837116cc
Varun Ramesh <varunramesh@live.com>
2022-11-24 17:07:04 -0700

    Remove codec2 as submodule

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Activate last Emscripten version.
+emsdk install 3.1.26
+emsdk activate 3.1.26
+
 mkdir -p build_wasm
 cd build_wasm
 emcmake cmake ../codec2
@@ -8,7 +12,7 @@ emmake make c2enc c2dec
 
 cd ..
 mkdir -p dist/
-cp build_wasm/src/c2dec.js dist
-cp build_wasm/src/c2dec.wasm dist
-cp build_wasm/src/c2enc.js dist
-cp build_wasm/src/c2enc.wasm dist
+cp build_wasm/src/c2dec.js build
+cp build_wasm/src/c2dec.wasm build
+cp build_wasm/src/c2enc.js build
+cp build_wasm/src/c2enc.wasm build
